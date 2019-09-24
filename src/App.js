455e51e16c0a465959269cc0774df294b3627488
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-09-24 23:04:50 +0700

    Update slide component part 1

--- src/App.js
+++ src/App.js
@@ -11,9 +11,15 @@ import Footer from './components/Footer';
 import Slide from './components/Slide';
 import FeatureProduct from './components/FeatureProduct';
 
+import slide1 from './img/slideshow1.jpg';
+import slide2 from './img/slideshow2.jpg';
+
 import { CartProvider } from './contexts/cart';
 
 const Index = () => <h2>Home</h2>
+const itemsUrl = [
+  slide1, slide2, slide1
+]
 
 function App() {
   return (
@@ -21,7 +27,7 @@ function App() {
       <Router>
         <div className="App">
           <TopMenu />
-          <Slide />
+          <Slide items={ itemsUrl } />
           <FeatureProduct />
 
           <Route path="/" exact component={Index} />
